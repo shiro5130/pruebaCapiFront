e32f1ccc8d139c7380ff86c4b8b00f7b62fd9b9b
shiro5130 <92557009+shiro5130@users.noreply.github.com>
2024-09-13 18:08:43 -0600

    proyecto

--- README.md
+++ README.md
@@ -1,27 +1,11 @@
-# NombreProyecto
+primero ocupamos instalar node_modules 
+con el comando
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.4.
+npm i
 
-## Development server
+y que este corriendo el servidor del back 
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+    private apiUrl = '{{rutadelback}}/api/contacts'; // URL del endpoint
+ y cambiar la ruta del back para su funcionamiento 
+ una ves cmabiado correr el proyecto con 
+ ng server
--- src/app/services/service.ts
+++ src/app/services/service.ts
@@ -1,21 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
+    private itemSource = new BehaviorSubject<any[]>([]);
+    items$ = this.itemSource.asObservable();
 
-    // private baseApiUrl = 'http://pruebacapi.test/api';
     private apiUrl = 'http://pruebacapi.test/api/contacts'; // URL del endpoint
+
     constructor(private http: HttpClient) { }
 
-    getData(): Observable<any> {
-        return this.http.get<any>(this.apiUrl);
+    // Obtener datos con paginación
+    getData(page: number = 1): Observable<any> {
+        return this.http.get<any>(`${this.apiUrl}?page=${page}`);
+    }
+    getDatafilter(page: number = 1, palabra: string): Observable<any> {
+        return this.http.get<any>(`${this.apiUrl}?page=${page}&name=` + palabra);
     }
 
+    // Crear nuevo contacto
     createData(body: {}): Observable<any> {
         return this.http.post<any>(this.apiUrl, body);
+    }
+
+    // Editar contacto existente
+    editData(body: any): Observable<any> {
+        console.log(body);
+        return this.http.put<any>(`${this.apiUrl}/${body.id}`, body);
+    }
+
+    // Eliminar contacto por ID
+    deleteData(id: number): Observable<any> {
+        return this.http.delete<any>(`${this.apiUrl}/${id}`);
     }
 }
